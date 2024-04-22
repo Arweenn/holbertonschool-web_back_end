9136a2edfe2b534af212ff7c46e9b86fe3b6d52c
Arweenn <arwendumont25@outlook.fr>
2024-04-23 00:21:18 +0200

    Trying a different way

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -9,7 +9,4 @@ their sum as a float.
 def sum_list(input_list: list[float]) -> float:
     """Returns the sum of a list of floats."""
 
-    sum_input = 0.0
-    for input in input_list:
-        sum_input += input
-    return sum_input
+    return sum(input_list)
