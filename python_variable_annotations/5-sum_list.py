755559ffe2e2b026a6236f5b40a1d781cea94727
Arweenn <arwendumont25@outlook.fr>
2024-04-23 00:19:41 +0200

    Fixing variable name

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -9,7 +9,7 @@ their sum as a float.
 def sum_list(input_list: list[float]) -> float:
     """Returns the sum of a list of floats."""
 
-    sum = 0.0
+    sum_input = 0.0
     for input in input_list:
-        sum += input
-    return sum
+        sum_input += input
+    return sum_input
