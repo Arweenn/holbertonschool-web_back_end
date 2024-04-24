2d26a5dfd97cbb65d44e1ace72114ce23ce3542d
Arweenn <arwendumont25@outlook.fr>
2024-04-24 10:26:24 +0200

    shebang fixed

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env/python3
 """
 Type-annotated function sum_mixed_list that
 takes a list mxd_lst of int and floats and
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env/python3
 """
 Annotates the parameters of the
 function below.
