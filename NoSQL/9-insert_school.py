f69ecbfbcc319185d6d3f248229f0505bbe5774b
Arweenn <arwendumont25@outlook.fr>
2024-05-01 22:46:02 +0200

    Changed the 'insert_id' to the correct version of it

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,5 +7,5 @@ Python function that inserts a new document in a collection.
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection"""
 
-    new_id = mongo_collection.insert_one(kwargs)
-    return new_id.insert_id
+    new_id = mongo_collection.insert_one(kwargs).inserted_id
+    return new_id
