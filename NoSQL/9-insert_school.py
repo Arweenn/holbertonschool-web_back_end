b8f5edff3478b6ff144f05769d76708dcb2e85de
Arweenn <arwendumont25@outlook.fr>
2024-05-01 22:31:19 +0200

    First commit for the task 9 - Insert a document in Python

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Python function that inserts a new document in a collection.
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection"""
+
+    new_id = mongo_collection.insert_one(kwargs)
+    return new_id.insert_id
