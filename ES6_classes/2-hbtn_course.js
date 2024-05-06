5e27916ef5c56a84b01ff52292a6757955045fae
Arweenn <arwendumont25@outlook.fr>
2024-05-06 15:10:02 +0200

    First commit for the task 2 - A Course, Getters, and Setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = '', name;
+    this.length = 0, length;
+    this.students = students, students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+        this._name = value;
+    } else {
+        throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+        this._length = value;
+    } else {
+        throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (Array.isArray(value) && value.every((item) => typeof item === 'string')) {
+        this._students = value;
+    } else {
+        throw new TypeError('Students must be an array of string');
+    }
+  }
+}
