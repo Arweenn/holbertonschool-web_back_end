7d3d89a2d82eabe0fb1cb7a6230a770f884bfd30
Arweenn <arwendumont25@outlook.fr>
2024-05-07 10:34:22 +0200

    First commit for the task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = 0;
+    this.currency = new Currency('', '');
+
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+      throw new TypeError('Amount and Conversion Rate must be numbers');
+  }
+}
