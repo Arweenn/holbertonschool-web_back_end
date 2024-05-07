754fe37d48b2099ea75f5f1a3b3f4e41fb0772f0
Arweenn <arwendumont25@outlook.fr>
2024-05-07 10:51:41 +0200

    First commit for the task 5 - A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+        if (!this.evacuationWarningMessage){
+            throw new Error('Class extending Building must override evacuationWarningMessage')
+        }
+    }
+    this._sqft = sqft;
+    }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
