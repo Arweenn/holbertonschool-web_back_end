95bcb174fb3b1265bf4d7ab4e7935802515dab6a
Arweenn <arwendumont25@outlook.fr>
2024-05-07 12:11:59 +0200

    Del a get

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -6,10 +6,6 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
   get floors() {
     return this._floors;
   }
