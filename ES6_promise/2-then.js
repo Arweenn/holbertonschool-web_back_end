3e87203932ac669eb4b61855ff6305ef42d3df07
Arweenn <arwendumont25@outlook.fr>
2024-05-14 10:47:21 +0200

    First commit for the task 2 - Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  })
+    .catch (() => Error());
+}
