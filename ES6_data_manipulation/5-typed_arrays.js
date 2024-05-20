479eccdafa94affb9bad6123bda985dc7000a221
Arweenn <arwendumont25@outlook.fr>
2024-05-20 11:53:48 +0200

    First commit for the task 5 - Typed Arrays

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
+}
