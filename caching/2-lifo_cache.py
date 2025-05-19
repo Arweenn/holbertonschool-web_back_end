8a3f66edf38b17c80d2aa9c25daee0d691205b27
Arweenn <arwendumont25@outlook.fr>
2025-05-19 09:34:23 +0200

    pycode

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -6,38 +6,38 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-  """ LIFO caching """
+    """ LIFO caching """
 
-  def __init__(self):
-    """ Constructor """
-    super().__init__()
-    self.queue = []
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.queue = []
 
-  def put(self, key, item):
-    """ Puts item in cache """
-    if key is None or item is None:
-      return
+    def put(self, key, item):
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
 
-    self.cache_data[key] = item
+        self.cache_data[key] = item
 
-    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-      if self.queue:
-        last = self.queue.pop()
-        del self.cache_data[last]
-        print("DISCARD: {}".format(last))
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.queue:
+                last = self.queue.pop()
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
 
-    if key not in self.queue:
-      self.queue.append(key)
-    else:
-      self.mv_last_list(key)
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
 
-  def get(self, key):
-    """ Gets item from cache """
-    return self.cache_data.get(key, None)
+    def get(self, key):
+        """ Gets item from cache """
+        return self.cache_data.get(key, None)
 
-  def mv_last_list(self, item):
-    """ Moves element to last idx of list """
-    length = len(self.queue)
-    if self.queue[length - 1] != item:
-      self.queue.remove(item)
-      self.queue.append(item)
+    def mv_last_list(self, item):
+        """ Moves element to last idx of list """
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -5,32 +5,32 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-  """ LRU caching """
+    """ LRU caching """
 
-  def __init__(self):
-    """ Constructor """
-    super().__init__()
-    self.queue = []
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.queue = []
 
-  def put(self, key, item):
-    """ Puts item in cache """
-    if key is None or item is None:
-      return
+    def put(self, key, item):
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
 
-    self.cache_data[key] = item
+        self.cache_data[key] = item
 
-    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-      removed = self.queue.pop(0)
-      del self.cache_data[removed]
-      print("DISCARD: {}".format(removed))
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            removed = self.queue.pop(0)
+            del self.cache_data[removed]
+            print("DISCARD: {}".format(removed))
 
-    if key in self.queue:
-      self.queue.remove(key)
-    self.queue.append(key)
+        if key in self.queue:
+            self.queue.remove(key)
+            self.queue.append(key)
 
-  def get(self, key):
-    """ Gets item from cache """
-    if key in self.queue:
-      self.queue.remove(key)
-      self.queue.append(key)
-    return self.cache_data.get(key, None)
+    def get(self, key):
+        """ Gets item from cache """
+        if key in self.queue:
+            self.queue.remove(key)
+            self.queue.append(key)
+        return self.cache_data.get(key, None)
