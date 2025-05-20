7b2ebb27b0232cfeebd2b846d3aa55f8b4df89e6
Arweenn <arwendumont25@outlook.fr>
2025-05-20 10:23:06 +0200

    coquille

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -99,6 +99,5 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-        hashed_password = _hash_password(password)
         self._db.update_user(user.id, hashed_password=_hash_password(password),
                              reset_token=None)
