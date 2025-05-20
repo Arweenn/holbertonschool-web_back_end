d2eb1f31df69322b6c90066ff3a8edaf22572e20
Arweenn <arwendumont25@outlook.fr>
2025-05-20 15:05:01 +0200

    correction

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -7,7 +7,6 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
@@ -22,12 +21,14 @@ class Config(object):
 app.config.from_object("1-app.Config")
 
 
-@babel.localeselector
 def get_locale():
     """
     Get the current locale
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route("/")
