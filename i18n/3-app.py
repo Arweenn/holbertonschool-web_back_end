a391ee4a002aa330cfe3a26c6453481ef7947734
Arweenn <arwendumont25@outlook.fr>
2025-05-20 15:21:39 +0200

    oups bis

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -38,5 +38,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
+babel = Babel(app, locale_selector=get_locale)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
