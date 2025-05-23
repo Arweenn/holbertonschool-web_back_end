7b2f9f814dbd41d5f7eba8c873e08110c7829e30
Arweenn <arwendumont25@outlook.fr>
2025-05-23 09:33:01 +0200

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 import redis
 
 
@@ -40,3 +40,33 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        store input data in dedis using a random key
+        and return a key
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        param a value from redis to str
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: int) -> int:
+        """
+        param a value from redis to int
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
