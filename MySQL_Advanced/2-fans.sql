6a8cbfb34da223ab5b11d31eb2e343b83c46a7d3
Arweenn <arwendumont25@outlook.fr>
2025-06-24 01:07:36 +0200

    :)

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 SELECT origin, nb_fans FROM metal_bands
-GROUP BY origin ORDER BY nb_fans DESC;
+GROUP BY origin
+ORDER BY nb_fans DESC;
