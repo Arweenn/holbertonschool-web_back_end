fd053cbec7fb7b38fb576b970ead0346f0030054
Arweenn <arwendumont25@outlook.fr>
2025-06-23 19:04:02 +0200

    :)

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    country ENUM ('US', 'CO', 'TN') NOT NULL
+);
