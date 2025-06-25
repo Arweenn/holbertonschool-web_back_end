eb20141171cf301f337394361ff46fcae74100bd
Arweenn <arwendumont25@outlook.fr>
2025-06-25 16:43:48 +0200

    :)

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,2 +1,7 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+function calculateNumber(a, b) {
+  const rounded_a = Math.round(a)
+  const rounded_b = Math.round(b)
+  return rounded_a + rounded_b
+}
+
 module.exports = calculateNumber;
--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,28 +1,23 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const assert = require('assert')
+const calculateNumber = require('./0-calcul.js')
 
-describe('calculateNumber', function() {
-  it('should return 4', () => {
+describe('Rounded addition', function() {
+  it('should add two non rounded numbers corectly', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.9, 3), 5);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
-  it('should return 6', () => {
-    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-  });
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.6), 6);
+  })
 
-  it('negative numbers', () => {
-    assert.strictEqual(calculateNumber(-1, 1), 0);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(-1, -1), -2);
-  });
+  it('shoud work on negative numbers', function () {
+    assert.strictEqual(calculateNumber(1, -3), -2);
+    assert.strictEqual(calculateNumber(-1, 3.7), 3);
+    assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
+  })
 
-  it('checks arguments', () => {
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
-  });
-});
+  it('should work with 0', function () {
+    assert.strictEqual(calculateNumber(1.5, 0), 2)
+    assert.strictEqual(calculateNumber(0, 4.5), 5)
+    assert.strictEqual(calculateNumber(0, 0), 0)
+  })
+})
