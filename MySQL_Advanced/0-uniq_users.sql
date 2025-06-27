de96ab224ce406c66de756b7e5372bb2741c05a8
Arweenn <arwendumont25@outlook.fr>
2025-06-27 15:28:23 +0200

    :)

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL
+-- creates a new table if not exists
+CREATE TABLE IF NOT EXISTS users 
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR (255) NOT NULL UNIQUE,
+	name VARCHAR (255)
 );
--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-SELECT origin, nb_fans FROM metal_bands
+-- SQL script that ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
